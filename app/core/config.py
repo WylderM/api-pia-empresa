92470b7efc8bb7493d7b56815bb6d5b049797a4a
wylderM <wyldermenezes@gmail.com>
2025-03-25 13:47:19 -0300

    feat: atualiza arquivo de deploy

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,16 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+import os
+
+env_file = ".env.test" if os.getenv("PYTEST_RUNNING") else ".env"
+load_dotenv(env_file)
 
 class Settings(BaseSettings):
     database_url: str
     secret_key: str
     algorithm: str
-    access_token_expires_minutes: int = 30
     redis_url: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=env_file)
 
 settings = Settings()
